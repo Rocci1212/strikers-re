5b1eff435b4047c2cd1372adc12fafd4b3fa408e
Feder2228 <107298134+Feder2228@users.noreply.github.com>
2025-08-22 21:40:36 +0200

    update training mode v1.5 STILL WIP

--- codes/training mode -- feder/training mode - v1.5/source.asm
+++ codes/training mode -- feder/training mode - v1.5/source.asm
@@ -8,7 +8,7 @@
 # Version 1.5
 # Exception vector area bytes used: 0x80001550-0x80001553
 # Known safe registers: r12, r11, r3, r5, r15, r16, r17, r18, r10
-# Useful register: r8 input bitfield
+# Useful registers: r8 input bitfield, r28 controller number (starting from 0)
 
 
 CHECK_INPUT_ENABLE_FOR_HOME:
@@ -61,9 +61,9 @@ MAKE_TEAM_DISAPPEAR:
     li r15, 4               # r15 is the loop counter. 4 iterations.
     lis r11, 0x4100         # r11 = float 6     Y coord we're sending everyone to        
     lis r10, 0xC1B0         # r10 = float -22   X coord we're sending home sidekicks to
-    lis r5, 0               # r5 = float 0      X coord we're sending the captain to
-    lis r16, 0x8056			# Load address for Player objects in r16
-    ori r16, r16, 0xA740
+    # r5 = float 0      X coord we're sending the captain to
+    lis r17, 0x8056			# Load address for Player objects in r16
+    ori r16, r17, 0xA740
     # PAL rev1: 8056A740
     # PAL rev2:
     # NTSC-U:
@@ -92,17 +92,17 @@ ITEM_STUFF:
     lbz r10, 0x1551 (r12)			# Load current selected item's quantity
     cmpwi r5, 0xff			# If r5 = 0xff then it becomes 0xffffffff, otherwise it would break the game
     bne SET_ITEM_QUANTITY_NOT_ZERO
-    li r5, 0xFFFFffff
+    li r5, 0xFFFFFFFF
 
 SET_ITEM_QUANTITY_NOT_ZERO:
     cmpwi r10, 0			# If r10 = 0 then certain items won't work because quantity = 0
-    bne DOUBLE_INPUT_CHECK
+    bne CHECK_IF_INPUTS_LOCKED
     li r10, 1
 
-DOUBLE_INPUT_CHECK:
-    lbz r16, 0x1552 (r12)			# Load a value that tells the code if a button was pressed in the last frame
-    cmpwi r16, 0
-    bne- DOUBLE_INPUT_DETECT
+CHECK_IF_INPUTS_LOCKED:
+    lbz r16, 0x1552 (r12)      # load controller number of who pressed dpad on this or on last frame. 4 if nobody
+    cmpwi r16, 4
+    bne- CHECK_IF_SAME_CONTROLLER
 
 CHECK_INPUT_LEFT:
     cmpwi r8, 0x0101 			# Check if 2 and D-Pad Left are pressed
@@ -129,46 +129,56 @@ CHECK_INPUT_DOWN:
 
 CHECK_INPUT_UP:
     cmpwi r8, 0x0108 			# Check if 2 and D-Pad Up are pressed
-    bne+ DOUBLE_INPUT_DETECT
+    bne+ CHECK_IF_SAME_CONTROLLER
 	cmpwi r10, 25               # if quantity is 25 (maximum), skip this
-	bge DOUBLE_INPUT_DETECT
+	bge CHECK_IF_SAME_CONTROLLER
 	addi r10, r10, 1            # increment quantity
 
-DOUBLE_INPUT_DETECT:
-    li r16, 0
-    andi. r0, r8, 0x000f	# Do a bitwise AND between r8 and 0x000f. Each of the 4 d-pad buttons sets one of the last bits high and we want the code to take the branch if none is pressed.
-    beq+ STORE_ITEMS
-	li r16, 1			# D-PAD HAS BEEN PRESSED ON THIS FRAME, therefore player's input will be ignored starting from the next frame until the player releases the button.
+CHECK_IF_SAME_CONTROLLER:
+    cmpwi r16, 4            # If nobody pressed dpad on last frame, check if somebody is now.
+    beq DETERMINE_IF_SHOULD_LOCK_INPUTS
+    cmpw r16, r28           # If this is not the same controller that started holding d-pad, skip this. The input lock byte will stay as is.
+    bne UPDATE_INPUT_LOCK_BYTE 
+    li r16, 4               # If this is the same controller that started holding d-pad, check if is still holding. If not store 4 at 80001552
 
-STORE_ITEMS:
+DETERMINE_IF_SHOULD_LOCK_INPUTS:
+    andi. r0, r8, 0x000f	# Do a bitwise AND between r8 and 0x000f. Each of the four d-pad buttons sets one of the last bits high and we want the code to take the branch if none is pressed.
+    beq+ UPDATE_INPUT_LOCK_BYTE
+    # D-PAD HAS BEEN PRESSED ON THIS FRAME, therefore player's input will be ignored starting from the next frame until the player who pressed the button releases it.
+	
+LOCK_INPUTS:
+    mr r16, r28			    # If a d-pad button was pressed by this controller, save its number
+
+UPDATE_INPUT_LOCK_BYTE:
+    stb r16, 0x1552 (r12)	# Update the value at 0x80001552
+
+SAVE_ITEM_SELECTION:
     stb r5, 0x1550	(r12)		# Store the item you picked into 0x80001550
     stb r10, 0x1551	(r12)		# Store item quantity into 0x80001551
-    stb r16, 0x1552 (r12)		# Update the value in 0x80001552
-    li r11, 0xFFFFffff	        # r11 = -1
-    lis r16, 0x806e
-    lwz r16, 0xFFFFf9d8 (r16)	# Load pointer to the struct that contains the player's item
+    li r11, 0xFFFFffff	        # r11 = -1. Away will have its items removed
+    bgt cr7, GIVE_ITEMS         # Check if training mode is on for home or if it's for away
+
+AWAY_GETS_ITEMS:
+    mr r11, r5                  # Give items to away
+    li r5, 0xFFFFffff	        # r5 = -1. Home will have its items removed
+
+GIVE_ITEMS:
+    lis r16, 0x806E
+    lwz r16, 0xFFFFF9D8 (r16)	# Load pointer to the struct that contains the player's item
     # PAL rev1: 806DF9D8
     # PAL rev2:
     # NTSC-U:
     # NTSC-J:
     # NTSC-K:
-    beq cr7, GIVE_ITEM_TO_AWAY # Check if training mode is on for home or if it's for away
-
-GIVE_ITEM_TO_HOME:
-    stw r5, 0x8c (r16)		    # Store home's new item
+    stw r5, 0x8C (r16)		    # Store home's new item
     stw r10, 0x90 (r16)		    # Store home's new item quantity
-    stw r11, 0xbf4 (r16)	    # Remove away's items
+    stw r11, 0xBF4 (r16)	    # Remove away's items
+    stw r10, 0xBF8 (r16)        # away quantity
     b END
 
-GIVE_ITEM_TO_AWAY:
-    stw r5, 0xbf4 (r16)			# Store away's new item
-    stw r10, 0xbf8 (r16)		# Store away's new item quantity
-    stw r11, 0x8c (r16)			# Remove home's items
-    b END
 
 DISABLE_TRAINING_MODE:
-    li r3, 0
-    stb r3, 0x1553 (r12)			# SET TRAINING MODE STATUS TO 0 (Off)
+    stb r12, 0x1553 (r12)			# SET TRAINING MODE STATUS TO 0 (Off)
 
 END:
     lwz	r3, 0 (r31)	# Original instruction
